a0fd3e43158b0811393f8081d2ae810e305c26df
VoSyThien <74333568+VoSyThien@users.noreply.github.com>
2020-12-03 10:11:02 +0700

    Create tong.C

---
+++ Bài tập 2/tong.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai báo bien a, b
+  int a, b;
+  //hien thi nhap a =
+  printf("nhap a = ");
+  scanf("%d", &a);
+  //hien thi nhap b =
+  printf("nhap b = ");
+  scanf("%d", &b);
+  //hien thi tong a + b
+  printf("a + b = %d", a + b);
+  return 0;
+}
