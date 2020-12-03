422640ea44e6395f2e21c653b9646415a52f3b6b
VoSyThien <74333568+VoSyThien@users.noreply.github.com>
2020-12-03 10:41:54 +0700

    Create khoiluong.C

---
+++ Bài tập 2/khoiluong.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int m;
+  scanf("%d", &m);
+  printf("m = %dg\n", m * 1000);
+  printf("KG | G\n");
+  printf("%d", m);
+  printf(" |%d", m * 1000);
+  return 0;
+}
