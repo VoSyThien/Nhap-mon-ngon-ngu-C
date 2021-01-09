e634076a4cac14448a9cd272611289cfe567d30a
VoSyThien <74333568+VoSyThien@users.noreply.github.com>
2021-01-09 10:16:39 +0700

    Create kt_06a.c

---
+++ Bài tập 3/kt_06a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  //h = 4, 4 hang
+  //hang 1: 1 sao,3 space
+  //hàng 2: 3 sao , 2 space
+  //hanh 3: 5 sao, 1 space
+  //hang 4: 7 sao, 0 space
+  
+  //h = 3
+  //hang 1: 1 sao, 2 space
+  //hang 2: 3 sao, 1 space
+  //hang 3: 5 sao, 0 space
+
+  //h = 2
+  //hang 1: 1 sao, 1 space
+  //hang 2: 3 sao, 0 space
+  printf("Hello World\n");
+  return 0;
+}
